9d7767b112dfae93c431bb95ce52fbd6696e6009
PeterNguyen <peter.nguyen@vmodev.com>
2020-06-24 17:28:23 +0700

    add action and thunk

---
+++ src/actions/index.js
@@ -0,0 +1,43 @@
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS;
+export const SELECT_SUBREDDIT= 'SELECT_SUBREDDIT';
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBBREDDIT';
+
+export const selectSubreddit = subreddit => ({
+  type: SELECT_SUBREDDIT,
+  subreddit,
+});
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
+  subreddit,
+});
+
+export const rquestPostss = subreddit => ({
+  type: REQUEST_POSTS,
+  subreddit,
+});
+
+const fetchPosts = subreddit => dispatch => {
+  dispatch(requestPosts(subreddit));
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+    .then(response => response.json())
+    .then(json => dispatch(receivePosts(subreddit, json)))
+};
+
+const shouldFetchPosts = (state, subreddit) => {
+  const posts = state.postsBySubreddit[subreddit];
+  if (!posts) {
+    return true;
+  }
+  if (posts.isFetching) {
+    return false;
+  }
+  return posts.didInvalidate;
+};
+
+export const receivePosts = subreddit =>  (dispatch, getState) => {
+  if (shouldFetchPosts(getState(), subreddit)) {
+    return dispatch(fetchPosts(subreddit));
+  }
+};
